d4e027027a3a9b3b649c5c5f10bb4836c8cb0c9c
giralli <hilary5august.leo@gmail.com>
2022-02-11 10:22:06 -0600

    PrecioMayor

---
+++ src/PrecioMayor.java
@@ -0,0 +1,17 @@
+package Libro;
+
+import java.util.Comparator;
+
+public class PrecioMayor implements Comparator<Libro> {
+	@Override
+	public int compare(Libro l1, Libro l2) {
+		if(l1.getPrecio()>l2.getPrecio()) {
+			return -1;
+		}
+		if(l1.getPrecio()<l2.getPrecio()) {
+			return 1;
+		}
+		else {
+			return 0;}}
+
+}
